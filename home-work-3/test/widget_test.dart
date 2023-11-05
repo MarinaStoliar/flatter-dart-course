77641648e65c4fcdcc41ee94922412b3f9253820
maryna.stoliar <maryna.stoliar@infotech.gov.ua>
2023-11-05 20:50:55 +0200

    edit home-work-2 task 2

--- home-work-3/test/widget_test.dart
+++ home-work-3/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:home_work_3/main.dart';
+import 'package:home_work_3/main-1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
