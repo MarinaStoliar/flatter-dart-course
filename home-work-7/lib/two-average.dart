5929ed0d08e01b45e0de749eb714d5797a6d7175
maryna.stoliar <maryna.stoliar@infotech.gov.ua>
2024-01-06 23:11:21 +0200

    add home-work-7 task 2

--- home-work-7/lib/two-average.dart
+++ home-work-7/lib/two-average.dart
@@ -1,1 +1,41 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+
+void main() {
+  double radius = 0.0;
+  double width = 0.0;
+  double height = 0.0;
+
+  // Обрати фігуру
+  int choice = 3;
+
+  switch (choice) {
+    case 1:
+      calculateArea('Circle', 2.5, 0, 0);
+      break;
+    case 2:
+      calculateArea('Square', 0, 7, 0);
+      break;
+    case 3:
+      calculateArea('Rectangle', 0, 7, 10);
+      break;
+    default:
+      print("Невірний вибір");
+      return;
+  }
+}
+
+void calculateArea(String currentShape, double radius, double width, double height) {
+  double area;
+  if (currentShape == 'Circle') {
+    area = 3.14 * radius * radius;
+  } else if (currentShape == 'Square') {
+    area = width * width;
+  } else if (currentShape == 'Rectangle') {
+    area = width * height;
+  } else {
+    area = 0;
+  }
+
+  print("Площа фігури $currentShape: $area");
+}
+
