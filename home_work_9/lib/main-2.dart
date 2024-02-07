ac847600d6eb080d2afc551f4dc3e6376db9cfae
MarinaStoliar <stoliarmarina39@gmail.com>
2024-02-07 17:36:27 +0200

    add task 1-2

--- home_work_9/lib/main-2.dart
+++ home_work_9/lib/main-2.dart
@@ -1,27 +1,39 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
-void main() async {
-  var timer = MyTimer();
-  timer.countDown(10).listen((event) {
-    print('New count: $event');
-  });
+class ParameterSender {
+  int voltage = 220;
+  int power = 3200;
+  int speed = 0;
+  int silentMode = 0;
+
+  late Timer _timer;
+
+  ParameterSender() {
+    _timer = Timer.periodic(Duration(seconds: 2), (Timer timer) {
+      sendMessage();
+    });
+  }
+
+  void updateParameters(int newVoltage, int newPower, int newSpeed, int newSilentMode) {
+    voltage = newVoltage;
+    power = newPower;
+    speed = newSpeed;
+    silentMode = newSilentMode;
+  }
+
+  void sendMessage() {
+    if (_hasParametersChanged) {
+      print('<voltage=$voltage,power=$power,speed=$speed,silent_mode=$silentMode>');
+    }
+  }
+
+  bool get _hasParametersChanged {
+    return true;
+  }
 }
 
-class MyTimer{
-
-  Stream<int> countDown(int seconds){
-    int secondsLeft = seconds;
-    var controller = StreamController<int>();
-
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      controller.add(secondsLeft);
-      if(secondsLeft) {
-        secodsLeft--;
-      } else {
-        timer.cancel();
-      }
-    });
-
-  }
+void main() {
+  ParameterSender parameterSender = ParameterSender();
+  parameterSender.updateParameters(220, 3200, 0, 0);
+  parameterSender.updateParameters(10, 1000, 5, 5);
 }
