f2961e4b2837cf7496c56941df3bba78446126c0
MarinaStoliar <stoliarmarina39@gmail.com>
2024-03-24 20:42:34 +0200

    add task 1

--- module_2_home_work_2/lib/main.dart
+++ module_2_home_work_2/lib/main.dart
@@ -1,23 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Directionality(
+    textDirection: TextDirection.ltr,
+    child: const MenuWidget(),
+  ),);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MenuWidget extends StatelessWidget {
+  const MenuWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(top: 32),
-            child: Text('MyText', textScaleFactor: 2, )
-          )
-        )
+    return Container(
+      color: Colors.blueGrey,
+      width: double.infinity,
+      child: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 4),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _MenuWidgetRow(
+                icon: Icons.home,
+                text: 'Home',
+            ),
+            _MenuWidgetRow(
+              icon: Icons.favorite,
+              text: 'Favorite',
+            ),
+            _MenuWidgetRow(
+              icon: Icons.add,
+              text: 'Add',
+            ),
+            _MenuWidgetRow(
+              icon: Icons.access_time ,
+              text: 'Access time',
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+class _MenuWidgetRow extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const _MenuWidgetRow({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(icon, size: 25),
+          SizedBox(width: 25),
+          Expanded(child: Text(text, style: TextStyle(fontSize: 20)))
+        ],
+      ),
+    );
+  }
+}
+
+
